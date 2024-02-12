564b7b5fb9988753831b6aa39032c3a38f3213ea
Antogin <georges@getjust.eu>
2024-02-12 21:52:54 +0100

    save

--- charli.js
+++ charli.js
@@ -118,7 +118,7 @@ function extractUrlsFromHtml(htmlString) {
 
         // console.log(urls); // Process the received data
 
-        const xcxUrl = urls.find(el => el.includes('tokyo'));
+        const xcxUrl = urls.find(el => el.includes('mumbai'));
 
 
         if(xcxUrl){
